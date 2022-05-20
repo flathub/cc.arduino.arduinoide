823bc65f2fcd2641c4e7a3922573f4938dfe3f6f
Joshua Glass <joshuag1000@outlook.com>
2022-05-20 17:07:13 +0100

    I cracked zypak's secrets Zypak should work now meaning that --no-sandbox is nolonger needed still struggling with compile from source but i will get there in the future.

--- arduino-ide.sh
+++ arduino-ide.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-FLAGS='--no-sandbox'
+FLAGS=''
 
 if [[ $XDG_SESSION_TYPE == "wayland" ]] && [ -c /dev/nvidia0 ]
 then
     FLAGS="$FLAGS --disable-gpu-sandbox"
 fi
 
-# I'm Unable to get this working just yet.
-#ELECTRON_RUN_AS_NODE=1
-#ZYPAK_SPAWN_LATEST_ON_REEXEC=0
-#env TMPDIR=$XDG_CACHE_HOME zypak-wrapper /app/arduino-ide/arduino-ide $FLAGS "$@"
-
-exec /app/arduino-ide/arduino-ide $FLAGS "$@"
+# I cracked zypak's secrets
+export ZYPAK_SPAWN_LATEST_ON_REEXEC=0
+env TMPDIR=$XDG_CACHE_HOME zypak-wrapper /app/arduino-ide/arduino-ide /app/arduino-ide/resources/app/src-gen/frontend/electron-main.js $FLAGS "$@"
